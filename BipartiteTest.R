18744b6b77c6b5d492f17cd94f25340f6eb0eb7f
John B. Buncher <jbuncher@gmail.com>
2017-05-30 16:37:52 -0500

    create bipartite network projections and plot with edge thickness as weights

--- BipartiteTest.R
+++ BipartiteTest.R
@@ -6,3 +6,9 @@ TestBipartiteMatrix <- read.csv("/media/jbuncher/Research/PER/REU/2017/R/MAMCR/T
 
 # convert the matrix we imported into an igraph object
 testNetwork <- graph_from_incidence_matrix(TestBipartiteMatrix, weighted = T)
+
+# create the two bipartite projections from the network
+testNetwork.bp <- bipartite.projection(testNetwork)
+
+# plots the network of answer selections where the edge width is given by the edge weight
+plot(testNetwork.bp$proj2, edge.width =E(testNetwork.bp$proj2)$weight)
