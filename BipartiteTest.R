f58b491d79a687b0d87eb835015b5f670c384911
John B. Buncher <jbuncher@gmail.com>
2017-05-30 16:20:43 -0500

    added comments

--- BipartiteTest.R
+++ BipartiteTest.R
@@ -1,4 +1,8 @@
 library(readr)
 library(igraph)
+
+# import the csv file which has the bipartite network in matrix form
 TestBipartiteMatrix <- read.csv("/media/jbuncher/Research/PER/REU/2017/R/MAMCR/TestBipartiteMatrix.csv", header=T, as.is=T,row.names=1)
+
+# convert the matrix we imported into an igraph object
 testNetwork <- graph_from_incidence_matrix(TestBipartiteMatrix, weighted = T)
