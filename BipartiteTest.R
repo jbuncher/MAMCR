fffca18731b0ffdeb94798cc67fb732388251817
John B. Buncher <jbuncher@gmail.com>
2017-05-30 16:16:52 -0500

    added command to create graph object from the incidence matrix, with weights

--- BipartiteTest.R
+++ BipartiteTest.R
@@ -1,3 +1,4 @@
 library(readr)
 library(igraph)
 TestBipartiteMatrix <- read.csv("/media/jbuncher/Research/PER/REU/2017/R/MAMCR/TestBipartiteMatrix.csv", header=T, as.is=T,row.names=1)
+testNetwork <- graph_from_incidence_matrix(TestBipartiteMatrix, weighted = T)
