dc46812719e757dca6bb67bb88c4f41ba9c4eb06
John B. Buncher <jbuncher@gmail.com>
2017-06-12 14:02:50 -0500

    add code to automatically construct list of column names in final matrix and apply this to the matrix

--- DataConversionScript_Test.R
+++ DataConversionScript_Test.R
@@ -3,3 +3,23 @@
 # Import the File of Responses
 TestResponse <- read.csv("/media/jbuncher/Research/PER/REU/2017/R/MAMCR/TestResponse.csv",row.names=1)
 
+# Extract the number of questions and students from original data file
+NofQs = ncol(TestResponse)
+NofSs = nrow(TestResponse)
+
+# State the number of possible responses and create the letter choices
+NofPRs = 4
+choices = LETTERS[1:NofPRs]
+
+# Create empty Matrix
+ActualMatrix <- matrix(data = NA, nrow  = NofSs, ncol = NofQs)
+
+# Convert our matrix to a data frame
+TestMatrix <- data.frame(TestMatrix)
+
+# create list of column names
+ColumnNames <- character()
+for (q in 1:NofQs) ColumnNames <- c(ColumnNames,paste0(rep(q,NofPRs),choices))
+
+# Give the columns in our data frame actual names
+TestMatrix <- setNames(TestMatrix, ColumnNames)
