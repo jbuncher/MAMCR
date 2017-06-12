ff105f9070488eccebba68ff13ae7f4097f28d63
John B. Buncher <jbuncher@gmail.com>
2017-06-12 13:42:08 -0500

    Initial Commit

---
+++ DataConversionScript_Test.R
@@ -0,0 +1,5 @@
+# Script to clean the "test" data file, converting choices (a, b, c, d) to "True/False"
+
+# Import the File of Responses
+TestResponse <- read.csv("/media/jbuncher/Research/PER/REU/2017/R/MAMCR/TestResponse.csv",row.names=1)
+
