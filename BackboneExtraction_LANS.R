648dd8a1688b979487f1f4d0dd0985a21bc37f60
John B. Buncher <jbuncher@gmail.com>
2017-06-29 12:57:53 -0500

    wrap the LANS code in a function named "LANS" that takes an igraph graph as input and returns an igraph graph as output

--- BackboneExtraction_LANS.R
+++ BackboneExtraction_LANS.R
@@ -13,42 +13,47 @@ testNetwork.bp <- bipartite.projection(testNetwork)
 # Store the response network as something shorter
 RN <- testNetwork.bp$proj2
 
-# Make the widths of the edges equal to the weights (for plotting)
-E(RN)$width <- E(RN)$weight
-
-# Remove all nodes not connected to the rest of the network, store as a new graph
-RN_C <- RN - V(RN)[degree(RN) == 0]
-
-# Add a "backbone" attribute to the edges in the network, with 1 indicating
-# that the edge is part of the backbone, and 0 otherwise.  Intialize to 0
-RN_C_BB <- set_edge_attr(graph = RN_C,name = "backbone",value = "0")
-
-# Get the number of vertices and edges so we don't have to keep computing them
-NofVs <- length(V(RN_C_BB))
-NofEs <- length(E(RN_C_BB))
-
-# makes a list of nodes and edges for convenience
-listOfNodes <- V(RN_C_BB)
-listOfEdges <- E(RN_C_BB)
-
-# for each node in the network....
-for (i in listOfNodes){
+LANS <- function(RN) {
+  # Make the widths of the edges equal to the weights (for plotting)
+  E(RN)$width <- E(RN)$weight
   
-  # grab the edges connected to that node and....
-  edgesForNode <- listOfEdges[from(listOfNodes[i])]
+  # Remove all nodes not connected to the rest of the network, store as a new graph
+  RN_C <- RN - V(RN)[degree(RN) == 0]
   
-  # find the value that 95% of the weights are >= and....
-  cutoff <- quantile(edgesForNode$weight,0.95)
+  # Add a "backbone" attribute to the edges in the network, with 1 indicating
+  # that the edge is part of the backbone, and 0 otherwise.  Intialize to 0
+  RN_C_BB <- set_edge_attr(graph = RN_C,name = "backbone",value = "0")
   
-  # mark those edges and ....
-  edgesToKeep <- edgesForNode[edgesForNode$weight >= cutoff]
+  # Get the number of vertices and edges so we don't have to keep computing them
+  NofVs <- length(V(RN_C_BB))
+  NofEs <- length(E(RN_C_BB))
   
-  # indicate that they are part of the backbone
-  E(RN_C_BB)[edgesToKeep]$backbone <- 1
+  # makes a list of nodes and edges for convenience
+  listOfNodes <- V(RN_C_BB)
+  listOfEdges <- E(RN_C_BB)
+  
+  # for each node in the network....
+  for (i in listOfNodes){
+    
+    # grab the edges connected to that node and....
+    edgesForNode <- listOfEdges[from(listOfNodes[i])]
+    
+    # find the value that 95% of the weights are >= and....
+    cutoff <- quantile(edgesForNode$weight,0.95)
+    
+    # mark those edges and ....
+    edgesToKeep <- edgesForNode[edgesForNode$weight >= cutoff]
+    
+    # indicate that they are part of the backbone
+    E(RN_C_BB)[edgesToKeep]$backbone <- 1
+  }
+  
+  # remove all edges not part of the backbone
+  final_backbone <- RN_C_BB - E(RN_C_BB)[backbone == 0]
+  
+  #return the backbone as output
+  return(final_backbone)
 }
-
-# remove all edges not part of the backbone
-final_backbone <- RN_C_BB - E(RN_C_BB)[backbone = 0]
 
 # plot the backbone!
 plot(final_backbone)
