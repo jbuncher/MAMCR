39ff77d42cabd6334b2868b794c9e8811638f89b
John B. Buncher <jbuncher@gmail.com>
2017-07-01 23:30:45 -0500

    add whitespace and remove unused variables as shown by RStudio code diagnostics

--- BackboneExtraction_LANS.R
+++ BackboneExtraction_LANS.R
@@ -2,7 +2,7 @@ library(readr)
 library(igraph)
 
 # import the csv file which has the bipartite network in matrix form
-TestBipartiteMatrix <- read.csv("/media/jbuncher/Research/PER/REU/2017/R/MAMCR/TestBipartiteMatrix.csv", header=T, as.is=T,row.names=1)
+TestBipartiteMatrix <- read.csv("/media/jbuncher/Research/PER/REU/2017/R/MAMCR/TestBipartiteMatrix.csv", header = T, as.is = T,row.names = 1)
 
 # convert the matrix we imported into an igraph object
 testNetwork <- graph_from_incidence_matrix(TestBipartiteMatrix, weighted = T)
@@ -24,16 +24,12 @@ LANS <- function(RN, alpha) {
   # that the edge is part of the backbone, and 0 otherwise.  Intialize to 0
   RN_C_BB <- set_edge_attr(graph = RN_C,name = "backbone",value = "0")
   
-  # Get the number of vertices and edges so we don't have to keep computing them
-  NofVs <- length(V(RN_C_BB))
-  NofEs <- length(E(RN_C_BB))
-  
   # makes a list of nodes and edges for convenience
   listOfNodes <- V(RN_C_BB)
   listOfEdges <- E(RN_C_BB)
   
   # for each node in the network....
-  for (i in listOfNodes){
+  for (i in listOfNodes) {
     
     # grab the edges connected to that node and....
     edgesForNode <- listOfEdges[from(listOfNodes[i])]
